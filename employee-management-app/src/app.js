4f9fcb20fd165ed17ec2cabc40a9c894b76e9b41
cankat güven <cankatguven@gmail.com>
2025-02-14 07:42:23 +0300

    icons file add

--- employee-management-app/src/app.js
+++ employee-management-app/src/app.js
@@ -1,46 +1,49 @@
-import { LitElement, html, css, unsafeCSS } from 'lit';
-import router from './router.js'; 
-import { brandColor } from '../constants.js';
-
+import { LitElement, html, css, unsafeCSS } from "lit";
+import router from "./router.js";
+import { brandColor } from "../constants.js";
 
 class EmployeeManagementApp extends LitElement {
   static styles = css`
+    .navbar {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
     nav {
       display: flex;
       justify-content: space-around;
       padding: 1rem;
-      background: ${unsafeCSS(brandColor)};
-      color: white;
+      gap:2rem
     }
     a {
-      color: white;
+      color: ${unsafeCSS(brandColor)};
       text-decoration: none;
       font-weight: bold;
     }
   `;
 
-
   render() {
     return html`
-      <nav>
-        <a href="/" @click="${this.navigate}">Home</a>
-      
-        <a href="/add" @click="${this.navigate}">Add Employee</a>
-      </nav>
+      <div class="navbar">
+      <img src="../assests/images/ING_logo.jpg" width=100> 
+        <nav>
+          <a href="/" @click="${this.navigate}">Employees</a>
+          <a href="/add" @click="${this.navigate}">Add Employee</a>
+        </nav>
+      </div>
       <main id="outlet"></main>
     `;
   }
 
   navigate(event) {
     event.preventDefault();
-    const href = event.target.getAttribute('href');
-  
+    const href = event.target.getAttribute("href");
+
     if (href && href !== window.location.pathname) {
-      router.render(href).catch(err => console.error('Routing error:', err));
-      window.history.pushState({}, '', href);
+      router.render(href).catch((err) => console.error("Routing error:", err));
+      window.history.pushState({}, "", href);
     }
   }
-  
 }
 
-customElements.define('employee-management-app', EmployeeManagementApp);
+customElements.define("employee-management-app", EmployeeManagementApp);
