1193f893b367464f6ab0bf3f2b744b6d1b5b61e6
cankat güven <cankatguven@gmail.com>
2025-02-15 14:46:35 +0300

    src edited

--- employee-management-app/src/state.js
+++
@@ -1,8 +0,0 @@
-export const saveState = (key, data) => {
-    localStorage.setItem(key, JSON.stringify(data));
-  };
-  
-  export const loadState = (key) => {
-    return JSON.parse(localStorage.getItem(key)) || [];
-  };
-  
